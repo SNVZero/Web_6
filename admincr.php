74d3cce6df9b4923bb971ef6cff756def7000e5b
SNVZero <simakov_nikita01@mail.ru>
2022-04-12 21:51:29 +0300

    first

--- admincr.php
+++ admincr.php
@@ -7,9 +7,13 @@ $hash = password_hash($password,PASSWORD_DEFAULT);
 $user = 'u46878';
 $pass = '2251704';
 $db = new PDO('mysql:host=localhost;dbname=u46878', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+try{
 
-
-$stmt = $db->prepare("INSERT INTO admin SET login = ?, password = ?");
-$stmt -> execute(array($login,$hash));
+    $stmt = $db->prepare("INSERT INTO admin SET login = ?, password = ?");
+    $stmt -> execute(array($login,$hash));
+}catch(PDOException $e){
+    print('Error : ' . $e->getMessage());
+    exit();
+}
 }
 ?>
