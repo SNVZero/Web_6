c29e3ed1a0f8f725aa6d07620fde1a8558cb7c7b
SNVZero <simakov_nikita01@mail.ru>
2022-06-01 17:22:06 +0300

    ad

--- myadmin/insert.php
+++ myadmin/insert.php
@@ -5,5 +5,5 @@
     $password = "admin";
     $hash = password_hash($password,PASSWORD_DEFAULT);
 
-    $stmt = $db->prepare("INSERT INTO admin SET login = ?, password = ?);
+    $stmt = $db->prepare("INSERT INTO admin SET login = ?, password = ?");
     $stmt -> execute([$login,$hash]);
