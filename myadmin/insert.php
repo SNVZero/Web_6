1514bf8e727042207114963f662ee3c8b4bbfcde
SNVZero <simakov_nikita01@mail.ru>
2022-06-01 17:20:40 +0300

    ad

--- myadmin/insert.php
+++ myadmin/insert.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once "../connect/db.php"
+    require_once "../connect/db.php";
     $login = "admin";
     $password = "admin";
     $hash = password_hash($password,PASSWORD_DEFAULT);
